b47ba48b5a9f6692504d269d9fd5b871c9fb431f
alison <alison-bh@hotmail.com>
2023-02-11 13:03:30 -0300

    feat: cria login hamburguer na pagina de registreo

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -17,3 +17,22 @@ export const requestCadastro =()=>{
     })
 }
 requestCadastro()
+const logicHamburguer = ()=>{
+    const imgHamburguer =  document.querySelector('.hamburguer')
+    const container = document.querySelector('.containerHamburguer')
+    imgHamburguer.addEventListener('click',()=>{
+      if(!imgHamburguer.id){
+          imgHamburguer.src = '../../assets/close.svg' 
+          imgHamburguer.id = 'close'
+          container.id='goOpen'
+      }else{
+          imgHamburguer.src = '../../assets/open.svg'
+          imgHamburguer.id=''
+          container.id=''
+      }  
+  
+  
+  
+    })
+  }
+  logicHamburguer()
